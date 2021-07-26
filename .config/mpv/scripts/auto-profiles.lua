407d130d3765ca4201cecf7bd836214ffabec1c2
Kishan <k@kishan.sh>
2021-07-26 08:58:38 +0530

    mpv

--- .config/mpv/scripts/auto-profiles.lua
+++ .config/mpv/scripts/auto-profiles.lua
@@ -1,192 +1,220 @@
+-- This script automatically loads playlist entries before and after the
+-- the currently played file. It does so by scanning the directory a file is
+-- located in when starting playback. It sorts the directory entries
+-- alphabetically, and adds entries before and after the current file to
+-- the internal playlist. (It stops if it would add an already existing
+-- playlist entry at the same position - this makes it "stable".)
+-- Add at most 5000 * 2 files when starting a file (before + after).
+
 --[[
+To configure this script use file autoload.conf in directory script-opts (the "script-opts"
+directory must be in the mpv configuration directory, typically ~/.config/mpv/).
 
-Automatically apply profiles based on runtime conditions.
-At least mpv 0.21.0 is required.
+Example configuration would be:
 
-This script queries the list of loaded config profiles, and checks the
-"profile-desc" field of each profile. If it starts with "cond:", the script
-parses the string as Lua expression, and evaluates it. If the expression
-returns true, the profile is applied, if it returns false, it is ignored.
-
-Expressions can reference properties by accessing "p". For example, "p.pause"
-would return the current pause status. If the variable name contains any "_"
-characters, they are turned into "-". For example, "playback_time" would
-return the property "playback-time". (Although you can also just write
-p["playback-time"].)
-
-Note that if a property is not available, it will return nil, which can cause
-errors if used in expressions. These are printed and ignored, and the
-expression is considered to be false. You can also write e.g.
-get("playback-time", 0) instead of p.playback_time to default to 0.
-
-Whenever a property referenced by a profile condition changes, the condition
-is re-evaluated. If the return value of the condition changes from false or
-error to true, the profile is applied.
-
-Note that profiles cannot be "unapplied", so you may have to define inverse
-profiles with inverse conditions do undo a profile.
-
-Using profile-desc is just a hack - maybe it will be changed later.
-
-Supported --script-opts:
-
-    auto-profiles: if set to "no", the script disables itself (but will still
-                   listen to property notifications etc. - if you set it to
-                   "yes" again, it will re-evaluate the current state)
-
-Example profiles:
-
-# the profile names aren't used (except for logging), but must not clash with
-# other profiles
-[test]
-profile-desc=cond:p.playback_time>10
-video-zoom=2
-
-# you would need this to actually "unapply" the "test" profile
-[test-revert]
-profile-desc=cond:p.playback_time<=10
-video-zoom=0
+disabled=no
+images=no
+videos=yes
+audio=yes
 
 --]]
 
-local utils = require 'mp.utils'
+MAXENTRIES = 5000
+
 local msg = require 'mp.msg'
+local options = require 'mp.options'
+local utils = require 'mp.utils'
 
-local profiles = {}
-local watched_properties = {}   -- indexed by property name (used as a set)
-local cached_properties = {}    -- property name -> last known raw value
-local properties_to_profiles = {} -- property name -> set of profiles using it
-local have_dirty_profiles = false -- at least one profile is marked dirty
+o = {
+    disabled = false,
+    images = true,
+    videos = true,
+    audio = true
+}
+options.read_options(o)
 
--- Used during evaluation of the profile condition, and should contain the
--- profile the condition is evaluated for.
-local current_profile = nil
-
-local function evaluate(profile)
-    msg.verbose("Re-evaluate auto profile " .. profile.name)
-
-    current_profile = profile
-    local status, res = pcall(profile.cond)
-    current_profile = nil
-
-    if not status then
-        -- errors can be "normal", e.g. in case properties are unavailable
-        msg.info("Error evaluating: " .. res)
-        res = false
-    elseif type(res) ~= "boolean" then
-        msg.error("Profile '" .. profile.name .. "' did not return a boolean.")
-        res = false
-    end
-    if res ~= profile.status and res == true then
-        msg.info("Applying profile " .. profile.name)
-        mp.commandv("apply-profile", profile.name)
-    end
-    profile.status = res
-    profile.dirty = false
+function Set (t)
+    local set = {}
+    for _, v in pairs(t) do set[v] = true end
+    return set
 end
 
-local function on_property_change(name, val)
-    cached_properties[name] = val
-    -- Mark all profiles reading this property as dirty, so they get re-evaluated
-    -- the next time the script goes back to sleep.
-    local dependent_profiles = properties_to_profiles[name]
-    if dependent_profiles then
-        for profile, _ in pairs(dependent_profiles) do
-            assert(profile.cond) -- must be a profile table
-            profile.dirty = true
-            have_dirty_profiles = true
+function SetUnion (a,b)
+    local res = {}
+    for k in pairs(a) do res[k] = true end
+    for k in pairs(b) do res[k] = true end
+    return res
+end
+
+EXTENSIONS_VIDEO = Set {
+    'mkv', 'avi', 'mp4', 'ogv', 'webm', 'rmvb', 'flv', 'wmv', 'mpeg', 'mpg', 'm4v', '3gp'
+}
+
+EXTENSIONS_AUDIO = Set {
+    'mp3', 'wav', 'ogm', 'flac', 'm4a', 'wma', 'ogg', 'opus'
+}
+
+EXTENSIONS_IMAGES = Set {
+    'jpg', 'jpeg', 'png', 'tif', 'tiff', 'gif', 'webp', 'svg', 'bmp'
+}
+
+EXTENSIONS = Set {}
+if o.videos then EXTENSIONS = SetUnion(EXTENSIONS, EXTENSIONS_VIDEO) end
+if o.audio then EXTENSIONS = SetUnion(EXTENSIONS, EXTENSIONS_AUDIO) end
+if o.images then EXTENSIONS = SetUnion(EXTENSIONS, EXTENSIONS_IMAGES) end
+
+function add_files_at(index, files)
+    index = index - 1
+    local oldcount = mp.get_property_number("playlist-count", 1)
+    for i = 1, #files do
+        mp.commandv("loadfile", files[i], "append")
+        mp.commandv("playlist-move", oldcount + i - 1, index + i - 1)
+    end
+end
+
+function get_extension(path)
+    match = string.match(path, "%.([^%.]+)$" )
+    if match == nil then
+        return "nomatch"
+    else
+        return match
+    end
+end
+
+table.filter = function(t, iter)
+    for i = #t, 1, -1 do
+        if not iter(t[i]) then
+            table.remove(t, i)
         end
     end
 end
 
-local function on_idle()
-    if mp.get_opt("auto-profiles") == "no" then
+-- splitbynum and alnumcomp from alphanum.lua (C) Andre Bogus
+-- Released under the MIT License
+-- http://www.davekoelle.com/files/alphanum.lua
+
+-- split a string into a table of number and string values
+function splitbynum(s)
+    local result = {}
+    for x, y in (s or ""):gmatch("(%d*)(%D*)") do
+        if x ~= "" then table.insert(result, tonumber(x)) end
+        if y ~= "" then table.insert(result, y) end
+    end
+    return result
+end
+
+function clean_key(k)
+    k = (' '..k..' '):gsub("%s+", " "):sub(2, -2):lower()
+    return splitbynum(k)
+end
+
+-- compare two strings
+function alnumcomp(x, y)
+    local xt, yt = clean_key(x), clean_key(y)
+    for i = 1, math.min(#xt, #yt) do
+        local xe, ye = xt[i], yt[i]
+        if type(xe) == "string" then ye = tostring(ye)
+        elseif type(ye) == "string" then xe = tostring(xe) end
+        if xe ~= ye then return xe < ye end
+    end
+    return #xt < #yt
+end
+
+local autoloaded = nil
+
+function find_and_add_entries()
+    local path = mp.get_property("path", "")
+    local dir, filename = utils.split_path(path)
+    msg.trace(("dir: %s, filename: %s"):format(dir, filename))
+    if o.disabled then
+        msg.verbose("stopping: autoload disabled")
+        return
+    elseif #dir == 0 then
+        msg.verbose("stopping: not a local path")
         return
     end
 
-    -- When events and property notifications stop, re-evaluate all dirty profiles.
-    if have_dirty_profiles then
-        for _, profile in ipairs(profiles) do
-            if profile.dirty then
-                evaluate(profile)
+    local pl_count = mp.get_property_number("playlist-count", 1)
+    -- check if this is a manually made playlist
+    if (pl_count > 1 and autoloaded == nil) or
+       (pl_count == 1 and EXTENSIONS[string.lower(get_extension(filename))] == nil) then
+        msg.verbose("stopping: manually made playlist")
+        return
+    else
+        autoloaded = true
+    end
+
+    local pl = mp.get_property_native("playlist", {})
+    local pl_current = mp.get_property_number("playlist-pos-1", 1)
+    msg.trace(("playlist-pos-1: %s, playlist: %s"):format(pl_current,
+        utils.to_string(pl)))
+
+    local files = utils.readdir(dir, "files")
+    if files == nil then
+        msg.verbose("no other files in directory")
+        return
+    end
+    table.filter(files, function (v, k)
+        if string.match(v, "^%.") then
+            return false
+        end
+        local ext = get_extension(v)
+        if ext == nil then
+            return false
+        end
+        return EXTENSIONS[string.lower(ext)]
+    end)
+    table.sort(files, alnumcomp)
+
+    if dir == "." then
+        dir = ""
+    end
+
+    -- Find the current pl entry (dir+"/"+filename) in the sorted dir list
+    local current
+    for i = 1, #files do
+        if files[i] == filename then
+            current = i
+            break
+        end
+    end
+    if current == nil then
+        return
+    end
+    msg.trace("current file position in files: "..current)
+
+    local append = {[-1] = {}, [1] = {}}
+    for direction = -1, 1, 2 do -- 2 iterations, with direction = -1 and +1
+        for i = 1, MAXENTRIES do
+            local file = files[current + i * direction]
+            local pl_e = pl[pl_current + i * direction]
+            if file == nil or file[1] == "." then
+                break
+            end
+
+            local filepath = dir .. file
+            if pl_e then
+                -- If there's a playlist entry, and it's the same file, stop.
+                msg.trace(pl_e.filename.." == "..filepath.." ?")
+                if pl_e.filename == filepath then
+                    break
+                end
+            end
+
+            if direction == -1 then
+                if pl_current == 1 then -- never add additional entries in the middle
+                    msg.info("Prepending " .. file)
+                    table.insert(append[-1], 1, filepath)
+                end
+            else
+                msg.info("Adding " .. file)
+                table.insert(append[1], filepath)
             end
         end
     end
-    have_dirty_profiles = false
+
+    add_files_at(pl_current + 1, append[1])
+    add_files_at(pl_current, append[-1])
 end
 
-mp.register_idle(on_idle)
-
-local evil_meta_magic = {
-    __index = function(table, key)
-        -- interpret everything as property, unless it already exists as
-        -- a non-nil global value
-        local v = _G[key]
-        if type(v) ~= "nil" then
-            return v
-        end
-        -- Lua identifiers can't contain "-", so in order to match with mpv
-        -- property conventions, replace "_" to "-"
-        key = string.gsub(key, "_", "-")
-        -- Normally, we use the cached value only (to reduce CPU usage I guess?)
-        if not watched_properties[key] then
-            watched_properties[key] = true
-            mp.observe_property(key, "native", on_property_change)
-            cached_properties[key] = mp.get_property_native(key)
-        end
-        -- The first time the property is read we need add it to the
-        -- properties_to_profiles table, which will be used to mark the profile
-        -- dirty if a property referenced by it changes.
-        if current_profile then
-            local map = properties_to_profiles[key]
-            if not map then
-                map = {}
-                properties_to_profiles[key] = map
-            end
-            map[current_profile] = true
-        end
-        return cached_properties[key]
-    end,
-}
-
-local evil_magic = {}
-setmetatable(evil_magic, evil_meta_magic)
-
-local function compile_cond(name, s)
-    chunk, err = loadstring("return " .. s, "profile " .. name .. " condition")
-    if not chunk then
-        msg.error("Profile '" .. name .. "' condition: " .. err)
-        return function() return false end
-    end
-    return chunk
-end
-
-for i, v in ipairs(mp.get_property_native("profile-list")) do
-    local desc = v["profile-desc"]
-    if desc and desc:sub(1, 5) == "cond:" then
-        local profile = {
-            name = v.name,
-            cond = compile_cond(v.name, desc:sub(6)),
-            properties = {},
-            status = nil,
-            dirty = true, -- need re-evaluate
-        }
-        profiles[#profiles + 1] = profile
-        have_dirty_profiles = true
-    end
-end
-
--- these definitions are for use by the condition expressions
-
-p = evil_magic
-
-function get(property_name, default)
-    local val = p[property_name]
-    if val == nil then
-        val = default
-    end
-    return val
-end
-
--- re-evaluate all profiles immediately
-on_idle()
+mp.register_event("start-file", find_and_add_entries)
